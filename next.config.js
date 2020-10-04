ba400b3ddb3f819f906b8a43ced8f9df25ed871b
smogola <mogola.sangare@external.loccitane.com>
2020-10-04 16:14:12 +0200

    rewrite url

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = {
       return [
         // Advanced rewrite
         // Query object shape: { id: string } (in addition to dynamic route param)
-        { source: "/posts/:id", destination: "/posts/:id" }
+        { source: "/posts/:id", destination: "/api/posts/:id" }
       ];
     }
   }
