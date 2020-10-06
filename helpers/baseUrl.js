e67a5a89deea21f69f9961261aca47a9c262992e
smogola <mogola.sangare@external.loccitane.com>
2020-10-06 14:54:16 +0200

    [get info Process]

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,4 +1,4 @@
 const getUrlProduction = process.env.URL_PRODUCTION
 const baseUrl = process.env.NODE_ENV === 'production' ? getUrlProduction : "http://localhost:9000"
-console.log("baseUrl", getUrlProduction)
+console.log("baseUrl",process.env.NODE_ENV,  getUrlProduction)
 export default baseUrl
