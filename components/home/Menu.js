f8f558e9f451260724e0c2e24155cae3788e250d
smogola <mogola.sangare@external.loccitane.com>
2021-01-22 14:35:08 +0100

    Animation of Menu

--- components/home/Menu.js
+++ components/home/Menu.js
@@ -20,7 +20,10 @@ import {notify} from '../notify'
 let easing = [0.175, 0.85, 0.42, 0.96];
 
 const imageVariants = {
-  exit: { y: [-120, 0], opacity: [0.5, 1], transition: { duration: 0.5, ease: easing } },
+  exit: {
+    y: [-120, 0], opacity: [0.5, 1],
+    transition: { duration: 0.5, ease: easing }
+  },
   enter: {
     y: [0, 0],
     opacity: [1,1],
