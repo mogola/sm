2a82b8da1b3c9c32e7d65796178dcaa1f92e4a18
Mogola SANGARE <Mogola.SANGARE.ext@loccitane.com>
2021-03-20 10:27:19 +0100

    fix cat

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -10,11 +10,10 @@ import Menu from './../../components/home/Menu'
 import Footer from './../../components/home/Footer'
 import Masonry from './../../components/Masonry'
 import moment from 'moment'
-import {RouterTracking} from './../../components/router/ngprogress'
 import {motion, useViewportScroll } from 'framer-motion'
 import { NextSeo } from 'next-seo';
 import SectionsRecent from './../../components/home/SectionRecent'
-
+import { themeContextUser } from './../../context/contextUser'
 import {
     Container,
     Columns,
@@ -59,25 +58,44 @@ const backVariants = {
           transition: { staggerChildren: 0.05, staggerDirection: -1 }
       }
     }
-const Category = ({post, config, connect, categories})=>{
+const Category = ({post, config, filter = false, isadmin= false, connect, categories, allPost, allCats})=>{
+    const router = useRouter()
+    const {id} = router.query
     const [configs, setConfigs] = useState(config)
     const [isAdmin, setIsAdmin] = useState(connect)
     const [isAnim, setIsAnim] = useState(false)
-    const router = useRouter()
-    const {id} = router.query
-    //console.log("id of post", router.isFallback, getNextId, getPrevId)
+    const [getPost, setGetPost] = useState([])
+    const [allPosts, getAllPosts] = useState({})
+ console.log("allCats id cat", allCats, allCats.length)
 
     const ComponentCategory = ({animboolean}) => {
+
+      const filterPosts = (paramsId) => {
+        
+        const getIdCat = allPosts.find(post => {
+          console.log(post._id, paramsId);
+          return post._id === paramsId 
+        })
+
+        console.log(allPost);
+        setGetPost(getIdCat.posts)
+      }
+      
       return (
-        <motion.div variants={backVariants} className="motionWrapper" initial="exit" animate={animboolean ? "exit": "enter"} exit="exit">
         <Container breakpoint="fullhd" className="breadCategory">
           <Heading className="titleBreadCategory" size={3}> Autres categories : </Heading>
           <ul className="listBreadCategory">
           {categories.map((cat, i) => (
               <li key={i}>
-                  <Link key={i} href={`${cat.nameCategory}`} as={`${cat._id}`}>
+                  <Link key={i} href={`/category/${cat.nameCategory}`} as={`/category/${cat._id}`}>
                       <a onClick={() => {
                           animatePage()
+                          console.log('refresh page');
+                          console.log("click on category");
+                          console.log(allPost);
+                          const filterPost = allPost.find(post => post._id === cat._id)
+                          console.log(filterPost.posts)
+                          setGetPost(filterPost.posts)
                       }}
                       className="linkToCategories">{cat.nameCategory}</a>
                   </Link>
@@ -85,22 +103,29 @@ const Category = ({post, config, connect, categories})=>{
           ))}
           </ul>
       </Container>
-      </motion.div>
       )
     }
+
+
     if(router.isFallback){
         return(
             <h3>loading...</h3>
         )
     }
-
     useEffect(() => {
-       // RouterTracking(router)
-
+      console.log("isAnim", isAnim, id)
+      console.log(allPost, post.posts.length)
+      // let postId  = allCats.find(post => post._id === id)
+      //     let postsId = postId["posts"]
+      //     console.log("======posts======", postsId);
+      // setGetPost(getPost)
+      //setGetPost(postId)
     }, [])
 
+  
+
     const animatePage = () => {
-        setIsAnim(!isAnim)
+        setIsAnim(false)
     }
     return(<>
       <Menu
@@ -114,23 +139,177 @@ const Category = ({post, config, connect, categories})=>{
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      {post.posts.length > 0 &&
-      <motion.div variants={variantsUl} className="motionWrapper" initial="exit" animate={isAnim ? "exit": "enter"} exit="exit">
-        <SectionsRecent
-            title={post.nameCategory}
-            data={post.posts}
-            className="section-category"
-            isadmin={isAdmin === true ? true : false}
-            getcategories={categories}
-            filter={false}
-            component={<ComponentCategory animaboolean={isAnim} />}
-            />
-            </motion.div>
-        }
-        {post.posts.length === 0 &&
-        <> no posts</>
-        }
-
+      <Section className="section-category">
+      <Container fluid className="containerTitleSection">
+        </Container>
+            <Columns className="homeCategory">
+      <themeContextUser.Consumer>
+                {({userConnected, postsCategory}) => (
+                  <>
+                  <div>{postsCategory.map((item, i) => (
+                   <div key={i}>
+                     {item.posts.map((post, h) => (
+                         <React.Fragment key={h}>
+                             {h === 0 &&
+                                 <Container fluid className="mainProject onTopView" key={`${h}${post._id}`}>
+                                 <Columns.Column className="columnProject" size={12}>
+                                     <motion.div variants={backVariants} className="categoryMainFilter">
+                                     <Link
+                                         href={'/projet/[slug]'}
+                                         as={`/projet/${encodeURIComponent(post.title)}`}
+                                     >
+                                         <a>
+                                             <Image className="mainImageCategory" loading="lazy" rounded={false} src={post.imageMainPrincipal} />
+                                         </a>
+                                     </Link>
+                                     {item.nameCategory &&
+                                         <Heading className={`${!filter ? "singleTitleCategory" : ''} titleMainCategory filterCategory`} size={1}>
+                                         {item.nameCategory}
+                                         {filter === true && <motion.div
+                                             className={`${filterToggle ? "filterVisible" : "filterHidden"} filterCategoryStatic homesFilter`}
+                                             initial="enter"
+                                             animate="enter"
+                                             exit="exit"
+                                             variants={backVariants}
+                                             data-id={filterToggle}
+                                             >
+                                             <motion.ul variants={variantsUl}>
+                                                 {getcategories.map((item, i) => (
+                                                 <motion.li
+                                                 variants={variantsItem}
+                                                 key={h}>
+                                                     <Tag
+                                                         data-id={item._id}
+                                                         className={`tagCatAdmin ${selectedCategories(item._id) ? "active" : "inactive" }`}
+                                                         name={item.nameCategory}
+                                                         style={{opacity:`${selectedCategories(item._id) ? 1 : 0.5 }`}}
+                                                         onClick={(e) => {
+                                                         e.preventDefault()
+                                                         filterData(item._id, item.nameCategory, h)
+                                                         }}>
+                                                         {item.nameCategory}
+                                                     </Tag>
+                                                 </motion.li>
+                                                 ))}
+                                                 </motion.ul>
+                                             </motion.div>
+                                         }
+                                         </Heading>
+                                     }
+                                     </motion.div>
+                                     <Content className="info">
+                                         <Tag.Group className="tagGroupPost">
+                                             <Tag className="recentDate">
+                                                 {moment(post.date).locale('fr').format('MMMM YYYY', 'fr')}
+                                                 <span className="nbItem">{`0${h+1}`}</span>
+                                             </Tag>
+                                             {post.listCategory.map((tag, j) => (
+                                                 <Tag  key={`${j}${post._id}`}>{tag}</Tag>
+                                             ))}
+                                         </Tag.Group>
+                                     <motion.div  variants={backVariants} className="center-category">
+                                         <Heading className="subTitleMainProject" size={1}>
+                                         {post.title}
+                                         </Heading>
+                                         <Content className="contentText">
+                                             <p>
+                                             {post.subTextDescription.substring(1, 200)}
+                                             </p>
+                                         </Content>
+                                         </motion.div>
+                                         <div className="indexZone">
+                                             <div className="contentZone">
+                                             <span className="nbItem">{`0${h+1}`}</span>
+                                             <Link
+                                                     href={{
+                                                         pathname:'/projet/[slug]',
+                                                         query:{id: post._id},
+                                                     }}
+                                                     as={`/projet/${encodeURIComponent(post.title)}`}
+                                                 >
+                                                     <a className="linkSee">Voir le projet <span className="icoRight" width={26}></span></a>
+                                                 </Link>
+                                                 <>
+                                                 {
+                                                     isadmin &&
+                                                     <Link href={'/admin/manageproject/[id]'} as={`/admin/manageproject/${post._id}`}>
+                                                         <a className="updatePost">View Post</a>
+                                                     </Link>
+         
+                                                 }
+                                                 </>
+                                             </div>
+                                         </div>
+                                         </Content>
+                                     </Columns.Column>
+                                     <ComponentCategory animaboolean={isAnim} />
+                                     </Container>
+                             }
+                             {h !== 0 &&
+                                 <Container breakpoint="fullhd" fluid className="mainProject subMainProject" key={`${i}${post._id}`}>
+                                     <Columns.Column className="columnProject" size={12}>
+                                         <Content className="info secondary-pr">
+                                         <Tag.Group className="tagGroupPost">
+                                             <Tag className="recentDate">
+                                                 {moment(post.date).locale('fr').format('MMMM YYYY', 'fr')}
+                                                 <span className="nbItem">{h+1 > 9 ? `${i+1}` : `0${h+1}`}</span>
+                                             </Tag>
+                                             {post.listCategory.map((tag, j) => (
+                                                 <Tag key={`${j}${post._id}`}>{tag}</Tag>
+                                             ))}
+                                         </Tag.Group>
+                                             <motion.div className="center-category">
+                                             <Link
+                                                 href={'/projet/[slug]'}
+                                                 as={`/projet/${encodeURIComponent(post.title)}`}
+                                             >
+                                             <a>
+                                                 <Image loading="lazy" className="mainImageCategory" rounded={false} src={post.imageMainPrincipal} />
+                                                 </a>
+                                             </Link>
+                                             </motion.div>
+                                             <div className="indexZone">
+                                                 <div className="contentZone">
+                                                 <span className="nbItem">{h+1 > 9 ? `${h+1}` : `0${h+1}`}</span>
+                                                 </div>
+                                             </div>
+                                             </Content>
+                                             <Content className="wrapperPostProject">
+                                                 <div className="infoPostCategory">
+                                                     <Heading className="subTitleMainProject" size={1}>
+                                                     {post.title}
+                                                     </Heading>
+                                                     <Content className="contentText">
+                                                         <p>
+                                                         {post.subTextDescription.substring(1, 200)}
+                                                         </p>
+                                                     </Content>
+                                                 </div>
+                                                 <Link
+                                                     href={'/projet/[slug]'}
+                                                     as={`/projet/${encodeURIComponent(post.title)}`}
+                                                 >
+                                                     <a className="linkSee">Voir le projet <span className="icoRight" width={26}></span></a>
+                                                 </Link>
+                                                 {
+                                                     isadmin &&
+                                                     <Link href={'/admin/manageproject/[id]'} as={`/admin/manageproject/${post._id}`}>
+                                                         <a className="updatePost" href={`/admin/manageproject/${post._id}`}>View Post</a>
+                                                     </Link>
+                                                 }
+                                             </Content>
+                                         </Columns.Column>
+                                     </Container>
+                                 }
+                             </React.Fragment>
+                         ))}
+                   </div>
+                  ))}</div>
+                    </>
+                  )}
+            </themeContextUser.Consumer>
+            </Columns>
+            </Section>
         <Footer
           menu={configs.menuCategoryLink}
           data={configs}
@@ -143,43 +322,44 @@ const Category = ({post, config, connect, categories})=>{
 }
 
 export async function getStaticProps({params:{id}}) {
-    try{
-    const config = await fetch(`${baseUrl}/api/info`, {method: 'GET'})
-    const allConfig = await config.json()
-    console.log("config", allConfig)
-    const getCategory = await fetch(`${baseUrl}/api/categories`, {method: 'GET'})
-    const categoriesPost = await getCategory.json()
-    const posts = JSON.parse(JSON.stringify(categoriesPost))
-    console.log(posts.posts)
-    const getPostData = await posts.find(post => post._id === id)
-    const getCategoryList = await fetch(`${baseUrl}/api/categories`, {
-        method: "GET",
-      })
+  try {
+      const config = await fetch(`${baseUrl}/api/info`, {method: 'GET'})
+      const allConfig = await config.json()
 
-      const allCategory = await getCategoryList.json()
+      const getCategory = await fetch(`${baseUrl}/api/categories`, {method: 'GET'})
+      const categoriesPost = await getCategory.json()
+      const posts = JSON.parse(JSON.stringify(categoriesPost))
 
-    return {
-            props: {
-                post:JSON.parse(JSON.stringify(getPostData)),
-                config: JSON.parse(JSON.stringify(allConfig)),
-                categories: allCategory
-            }
-        }
+      const getPostData = await posts.find(post => post._id === id)
+      const getCategoryList = await fetch(`${baseUrl}/api/categories`, {
+          method: "GET",
+        })
+
+        const allCategory = await getCategoryList.json()
+
+        return {
+          props: {
+              allPost: posts,
+              post:JSON.parse(JSON.stringify(getPostData)),
+              config: JSON.parse(JSON.stringify(allConfig)),
+              categories: JSON.parse(JSON.stringify(allCategory))
+          }
+      }
     }
     catch(err){
-        console.log(err)
+      console.log("error lors de la dynamisation", err)
     }
+
+   
 }
 
 // This function gets called at build time
 export async function getStaticPaths() {
     // Call an external API endpoint to get posts
     try{
-        const getCategory = await fetch(`${baseUrl}/api/categories`, {method: 'GET'})
+       const getCategory = await fetch(`${baseUrl}/api/categories`, {method: 'GET'})
         const categoriesPost = await getCategory.json()
-        console.log('postssss', categoriesPost)
         const posts = JSON.parse(JSON.stringify(categoriesPost))
-        console.log('postssss', posts)
       // Get the paths we want to pre-render based on posts
       const paths = posts.map((category) => ({
         params: { id: category._id },
@@ -190,7 +370,7 @@ export async function getStaticPaths() {
       return { paths, fallback: false }
     }
     catch(err){
-      console.log(err)
+      console.log("err", err)
     }
   }
 
