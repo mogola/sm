45ea3eb46d1e2da9d469bbedb03e8a87acbba643
Mogola SANGARE <Mogola.SANGARE.ext@loccitane.com>
2021-03-22 16:24:23 +0100

    [fix build error generate page]

--- components/layout.js
+++ components/layout.js
@@ -16,15 +16,13 @@ export default function Layout({ children, none, home, portfolio, dashboard, pos
   const [dataConfigs, setDataConfig] = useState([])
   const [naming, setNaming] = useState()
   const [menu, setMenu] = useState()
-  const [imageHomeMain, setImageHomeMain] = useState()
-  const [titleMain, setTitleMain] = useState()
-  const [subTitleImage, setSubTitleImage] = useState()
   const [textScrollTop, setTextScrollTop] = useState()
   const [state, changeState] = useState({});
   const [textAvailable, setTextAvailable] = useState([])
-  const [onLoadingPage, setOnLoadingPage] = useState(true)
+  const [onLoadingPage, setOnLoadingPage] = useState(false)
   const [seoUrl, setSeoUrl] = useState()
   const [fixedMenu, setFixedMenu] = useState(false)
+
   let compareStorage = (initialStorage, newStorage) => {
     if(initialStorage === JSON.stringify(newStorage))
     return true
@@ -39,57 +37,61 @@ export default function Layout({ children, none, home, portfolio, dashboard, pos
   }
 
   const getData = async () => {
-    const getData = await fetch(`${baseUrl}/api/info`)
-    const data = await getData.json()
-    
-    setOnLoadingPage(false)
+    try {
+      const getData = await fetch(`${baseUrl}/api/info`)
+      const data = await getData.json()
+      setOnLoadingPage(false)
+      
+      await new Promise((resolve) => {
+          resolve(dataStorage(data))
+      }).then(result => {
+        const {
+          menuCategoryLink,
+        } = result
 
-    await new Promise((resolve) => {
-        resolve(dataStorage(data))
-    }).then(result => {
-      const {
-        menuCategoryLink,
-      } = result
+        setMenu(menuCategoryLink)
+        setTextScrollTop(textScrollTop)
 
-      setMenu(menuCategoryLink)
-      setTextScrollTop(textScrollTop)
+        let dataCfs = Object.entries(result)
 
-      let dataCfs = Object.entries(result)
+        let obj = [];
+        dataCfs.forEach(([key, value]) => {
+        obj.push({[key] : value})
+        });
 
-      let obj = [];
-      dataCfs.forEach(([key, value]) => {
-       obj.push({[key] : value})
-      });
+        const available = result.textAvailable.split(' ')
+        let arrayAvailable = []
+        let arrayConcat = [];
 
-      const available = result.textAvailable.split(' ')
-      let arrayAvailable = []
-      let arrayConcat = [];
+        available.forEach(key => {
+          let keyLetter = key.split('')
+          keyLetter.push(' ')
+          arrayAvailable.push(keyLetter)
+        })
 
-      available.forEach(key => {
-        let keyLetter = key.split('')
-        keyLetter.push(' ')
-        arrayAvailable.push(keyLetter)
+        arrayAvailable.map((item, i) => {
+          let accItem = item
+          arrayConcat = [...arrayConcat, ...accItem]
+        })
+
+        setTextAvailable(arrayConcat)
+
+        dataCfs.forEach(([key, value])=> changeState(prevState => ({...prevState, [key]: value })));
+
+        setDataConfig(obj)
+        if(compareStorage(localStorage.getItem("info"),result)){
+          setDataConfig(localStorage.getItem("info"))
+        }else{
+          localStorage.setItem("info", JSON.stringify(result))
+          setDataConfig(JSON.stringify(result))
+        }
+
+        setOnLoadingPage(true)
       })
-
-      arrayAvailable.map((item, i) => {
-        let accItem = item
-        arrayConcat = [...arrayConcat, ...accItem]
-      })
-
-      setTextAvailable(arrayConcat)
-
-      dataCfs.forEach(([key, value])=> changeState(prevState => ({...prevState, [key]: value })));
-
-      setDataConfig(obj)
-      if(compareStorage(localStorage.getItem("info"),result)){
-        setDataConfig(localStorage.getItem("info"))
-      }else{
-        localStorage.setItem("info", JSON.stringify(result))
-        setDataConfig(JSON.stringify(result))
-      }
-
-      setOnLoadingPage(true)
-    })
+    }
+    catch(err){
+      console.log("layout err", err)
+    }
   }
 
   useEffect(() => {
@@ -168,164 +170,166 @@ export default function Layout({ children, none, home, portfolio, dashboard, pos
     </div>
   }
 
-  return (
-    <>
-    <themeContextUser.Consumer>
-      {({ dataConfig }) => (
-        <div data={naming} className={portfolio || none || post ? "df-none" : styles.container}>
-          {homepage && <NextSeo
-        facebook={{
-            appId: `${3587318871321107}`,
-          }}
-            title={siteTitle}
-            description={siteTitle}
-            canonical={`${baseUrl}`}
-            openGraph={{
-              url: `${baseUrl}`,
-              title: `${siteTitle}`,
-              description: `${siteTitle}`,
-              images: [
-                {
-                  url: "/app_visual_dewalgo.jpg",
-                  width: 800,
-                  height: 600,
-                  alt: 'Og Image Alt',
-                },
-                {
-                  url: "/app_visual_dewalgo.jpg",
-                  width: 900,
-                  height: 800,
-                  alt: 'Og Image Alt Second',
-                },
-                { url: "/app_visual_dewalgo.jpg" },
-                { url: "/app_visual_dewalgo.jpg" },
-              ],
-              site_name: `${name}`,
+  if(onLoadingPage){
+    return (
+      <>
+      <themeContextUser.Consumer>
+        {({ dataConfig }) => (
+          <div data={naming} className={portfolio || none || post ? "df-none" : styles.container}>
+            {homepage && <NextSeo
+          facebook={{
+              appId: `${3587318871321107}`,
             }}
-            twitter={{
-              handle: '@handle',
-              site: '@site',
-              cardType: 'summary_large_image',
-            }}
-          />}
-          <Head>
-            <link rel="icon" href="/favicon.ico" />
+              title={siteTitle}
+              description={siteTitle}
+              canonical={`${baseUrl}`}
+              openGraph={{
+                url: `${baseUrl}`,
+                title: `${siteTitle}`,
+                description: `${siteTitle}`,
+                images: [
+                  {
+                    url: "/app_visual_dewalgo.jpg",
+                    width: 800,
+                    height: 600,
+                    alt: 'Og Image Alt',
+                  },
+                  {
+                    url: "/app_visual_dewalgo.jpg",
+                    width: 900,
+                    height: 800,
+                    alt: 'Og Image Alt Second',
+                  },
+                  { url: "/app_visual_dewalgo.jpg" },
+                  { url: "/app_visual_dewalgo.jpg" },
+                ],
+                site_name: `${name}`,
+              }}
+              twitter={{
+                handle: '@handle',
+                site: '@site',
+                cardType: 'summary_large_image',
+              }}
+            />}
+            <Head>
+              <link rel="icon" href="/favicon.ico" />
 
-          </Head>
-          <header className={`${fixedMenu ? "fixedMain" : ""} header-portfolio ${styles.header}`}>
-            {home ? (
-              <>
-                <img
-                  src="/images/profile.png"
-                  className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                  rel="preload"
-                />
-                <h1 className={utilStyles.heading2Xl}>{state.nameSite}</h1>
-              </>
-            ) : dashboard ? (
-              <>
-                <h1 className={utilStyles.heading2Xl}>
-                  <span className={utilStyles.nameSite}>
-                    <Link href="/">
-                      <a>{state.nameSite}</a>
-                      </Link>
-                    </span><span>Créer un projet</span>
-                </h1>
-              </>
-            )
-            : (none || post) ? (<></>
-              // <>
-              //   {post &&
-              //   <div className="postBgSingle"
-              //   style={{backgroundColor:state.backgroudPost}}></div>}
-              // </>
-            )
-            : portfolio ? (
-              <>
-              <a className="menuB">
-                <span className="mTop"></span>
-                <span className="mBottom"></span>
-              </a>
-              <div  className="homeWrapper">
-                <div style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: "100%"
-                }}>
-                <div className="mainHeader">
-                    <div className="divHeader">
+            </Head>
+            <header className={`${fixedMenu ? "fixedMain" : ""} header-portfolio ${styles.header}`}>
+              {home ? (
+                <>
+                  <img
+                    src="/images/profile.png"
+                    className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                    rel="preload"
+                  />
+                  <h1 className={utilStyles.heading2Xl}>{state.nameSite}</h1>
+                </>
+              ) : dashboard ? (
+                <>
+                  <h1 className={utilStyles.heading2Xl}>
+                    <span className={utilStyles.nameSite}>
+                      <Link href="/">
+                        <a>{state.nameSite}</a>
+                        </Link>
+                      </span><span>Créer un projet</span>
+                  </h1>
+                </>
+              )
+              : (none || post) ? (<></>
+                // <>
+                //   {post &&
+                //   <div className="postBgSingle"
+                //   style={{backgroundColor:state.backgroudPost}}></div>}
+                // </>
+              )
+              : portfolio ? (
+                <>
+                <a className="menuB">
+                  <span className="mTop"></span>
+                  <span className="mBottom"></span>
+                </a>
+                <div  className="homeWrapper">
+                  <div style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: "100%"
+                  }}>
+                  <div className="mainHeader">
+                      <div className="divHeader">
+                        <h2 className={utilStyles.headingLg}>
+                          <Link href="/">
+                            <a className={`txtLogo ${utilStyles.colorInherit}`}>{state.nameSite}</a>
+                          </Link>
+                        </h2>
+                      </div>
+                      {menu &&
+                        <div className="mainMenu">
+                          <ul>
+                            {menu.map((item, i) => (
+                              <li key={i}>
+                                <Link href={item.url}>
+                                  <a className="menuLink">{item.name}</a>
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      }
+                    </div>
+                    <div className="titleBlockHome">
+                      <h3>
+                        {state.titleMain}
+                      </h3>
+                      <h3>
+                        {state.subTitleImage}
+                      </h3>
+                      <span className="categoryTextHome">Portfolio</span>
+                    </div>
+                    <div className="infoContact">
+                      <span className="emailUser">{state.email}</span>
+                      <span className="contactUser">{state.textLocalisation}</span>
+                    </div>
+                    <div>
+                    <div className="pastilleAvailable">
+                      {textAvailable.map((letter, i) => (
+                        <span key={i} className={`char${i + 1}`}>
+                          {letter}
+                        </span>
+                      ))}
+                    </div>
+                    </div>
+                    <a className="scrollTopLink">{state.textScrollTop}</a>
+                    </div>
+                  <div className="homeImage">
+                    <img data-img={state.menuCategoryLink} rel="preload" src={state.logoSiteUrl} width="100%" height="auto"/>
+                  </div>
+                </div>
+                </>
+              ) : (
+                    <>
                       <h2 className={utilStyles.headingLg}>
                         <Link href="/">
-                          <a className={`txtLogo ${utilStyles.colorInherit}`}>{state.nameSite}</a>
+                          <a className={utilStyles.colorInherit}>{name}</a>
                         </Link>
                       </h2>
-                    </div>
-                    {menu &&
-                      <div className="mainMenu">
-                        <ul>
-                          {menu.map((item, i) => (
-                            <li key={i}>
-                              <Link href={item.url}>
-                                <a className="menuLink">{item.name}</a>
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    }
-                  </div>
-                  <div className="titleBlockHome">
-                    <h3>
-                      {state.titleMain}
-                    </h3>
-                    <h3>
-                      {state.subTitleImage}
-                    </h3>
-                    <span className="categoryTextHome">Portfolio</span>
-                  </div>
-                  <div className="infoContact">
-                    <span className="emailUser">{state.email}</span>
-                    <span className="contactUser">{state.textLocalisation}</span>
-                  </div>
-                  <div>
-                  <div className="pastilleAvailable">
-                    {textAvailable.map((letter, i) => (
-                      <span key={i} className={`char${i + 1}`}>
-                        {letter}
-                      </span>
-                    ))}
-                  </div>
-                  </div>
-                  <a className="scrollTopLink">{state.textScrollTop}</a>
-                  </div>
-                <div className="homeImage">
-                  <img data-img={state.menuCategoryLink} rel="preload" src={state.logoSiteUrl} width="100%" height="auto"/>
-                </div>
+                    </>
+                  )}
+            </header>
+            <main className={post ? "wrapperPost" : ""}>{children}</main>
+            {/* {(!none || !post) && (
+              <div className={styles.backToHome}>
+                <Link href="/">
+                  <a className={utilStyles.linkInherit}>← Back to home</a>
+                </Link>
               </div>
-              </>
-            ) : (
-                  <>
-                    <h2 className={utilStyles.headingLg}>
-                      <Link href="/">
-                        <a className={utilStyles.colorInherit}>{name}</a>
-                      </Link>
-                    </h2>
-                  </>
-                )}
-          </header>
-          <main className={post ? "wrapperPost" : ""}>{children}</main>
-          {/* {(!none || !post) && (
-            <div className={styles.backToHome}>
-              <Link href="/">
-                <a className={utilStyles.linkInherit}>← Back to home</a>
-              </Link>
-            </div>
-          )} */}
-        </div>
-      )}
-    </themeContextUser.Consumer>
-    </>
-  )
+            )} */}
+          </div>
+        )}
+      </themeContextUser.Consumer>
+      </>
+    )
+  }
 }
