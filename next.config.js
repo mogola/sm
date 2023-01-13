8a8634692bab520bc1e1524bc47016d796ef8f11
smogola <mogola.sangare@external.loccitane.com>
2023-01-13 11:20:58 +0100

    [bug] fix error fs compilation

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
   // Target must be serverless
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.node = {
+        fs: 'empty'
+      }
     }
     return config;
   },
